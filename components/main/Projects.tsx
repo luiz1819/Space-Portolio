1a946acb4e4cefc8449b6ed9121f711a25df86e7
luiz carlos silva saccoman <123777496+luiz1819@users.noreply.github.com>
2025-07-12 16:23:13 -0400

    Fix image and Three.js errors

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
+          src="https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&w=1000&h=600&fit=crop"
           title="Modern Next.js Portfolio"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
